13082ed52244093f0222d9356d4633df544047dd
[RICHARD] <[4rnold.richard@gmail.com]>
2024-02-03 13:51:50 +0300

    first commit

---
+++ age calculator.py
@@ -0,0 +1,7 @@
+# an age calculator
+name = input("What is your name? ")
+current_year = int(input("Please input the current year? "))
+birth_year = int(input("What year were you born? "))
+age = current_year - birth_year
+print(name + " you are " + str(age) + " years old")
+# end
